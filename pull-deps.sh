1f5d3372112726f57db8c7462e64d5b76a80f535
Derek Steinmoeller <dsteinmo@gmail.com>
2017-12-16 13:38:37 -0500

    Fix umfpack dependency so we get the header files.

--- pull-deps.sh
+++ pull-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get install libblitz0-dev libblitz-doc libblitz0v5
-apt-get install libumfpack5.7.1
+apt-get install libsuitesparse-dev
 ln -s /usr/lib/x86_64-linux-gnu/libumfpack.so.5.7.1 /usr/lib/x86_64-linux-gnu/libumfpack.so
 
 curl -fSL https://github.com/joakimkarlsson/igloo/archive/igloo.1.1.1.tar.gz -o ./igloo.1.1.1.tar.gz
