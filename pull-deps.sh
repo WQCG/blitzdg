b949c7008965524d441adad4e0b37ce10e7e25fa
Derek Steinmoeller <dsteinmo@gmail.com>
2017-12-24 20:34:54 -0500

    Remove libblitz0v5 from pull-deps.sh, since travis can't find it.

--- pull-deps.sh
+++ pull-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get -y install libblitz0-dev libblitz-doc libblitz0v5
+apt-get -y install libblitz0-dev libblitz-doc
 apt-get -y install libsuitesparse-dev
 ln -s /usr/lib/x86_64-linux-gnu/libumfpack.so.5.7.1 /usr/lib/x86_64-linux-gnu/libumfpack.so
 apt-get -y install libmetis-dev libmetis-doc
