702e9252178c88038fe00ac8d9e492d8cca6469c
Derek Steinmoeller <dsteinmo@gmail.com>
2018-01-07 16:14:32 -0500

    Add keys for debian source.

--- pull-deps.sh
+++ pull-deps.sh
@@ -7,6 +7,10 @@ apt-get -y install libmetis-dev libmetis-doc
 apt-get -y install libboost-dbg libboost-dev libboost-doc
 apt-get -y install libarpack++2-dev
 
+wget -O - https://ftp-master.debian.org/keys/archive-key-8.asc | apt-key add -
+wget -O - https://ftp-master.debian.org/keys/archive-key-8-security.asc | apt-key add -
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
+
 echo "deb http://ftp.us.debian.org/debian jessie main" | sudo tee -a /etc/apt/sources.list
 apt-get -u update
 apt-get -y install libsuperlu4
