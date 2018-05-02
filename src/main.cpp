5775ed444122a47c1297ba78552974a9a518dbf4
Derek Steinmoeller <dsteinmo@gmail.com>
2018-05-01 21:40:08 -0400

    Check-in WIP for 1d avecttion solver.

--- src/main.cpp
+++ src/main.cpp
@@ -22,53 +22,56 @@ using namespace std;
 using namespace blitz;
 
 int main(int argc, char **argv) {
-	int N = 4;
-	int K = 10;
-	double xmin =-1.0;
+	
+  // Physical parameters
+  double xmin =-1.0;
 	double xmax = 1.0;
+  double c = 0.1;
 
+  const double finalTime = 10.0;
+  double t = 0.0;
+
+  // Numerical parameters:
+  int N = 4;
+	int K = 20;
+  double CFL = 0.5;
+
+  //Build dependencies.
   SparseMatrixConverter matrixConverter;
   EigenSolver eigenSolver(matrixConverter);
   DirectSolver directSolver(matrixConverter);
-
 	Nodes1DProvisioner nodes1DProvisioner(N, K, xmin, xmax, matrixConverter, eigenSolver, directSolver);
 	
+   
   nodes1DProvisioner.buildNodes();
-
   int Np = nodes1DProvisioner.get_NumLocalPoints();
-
-  Array<double,1> rGrid = nodes1DProvisioner.get_rGrid();
-  cout << rGrid << endl;
-
-  cout << nodes1DProvisioner.get_V() << endl;
-
-  Array<double, 2> DVr(Np, Np);
-
-  nodes1DProvisioner.computeGradVandermonde(DVr);
-
-  cout << DVr << endl;
-  cout << nodes1DProvisioner.get_Dr() << endl;
-
-  Array<double,2> x = nodes1DProvisioner.get_xGrid();
-
-  cout << x << endl;
-
+  
   Array<double,2> J(Np, K);
   Array<double,2> rx(Np, K);
-
   nodes1DProvisioner.computeJacobian(J, rx);
 
-  cout << "Can_Partition_A_Mesh" << endl;
-  MeshManager * manager = new MeshManager();
-  MeshManager & mgr = *manager;
-  mgr.readVertices("input/2box.V");
-  mgr.readElements("input/2box.E2V");
 
-  mgr.partitionMesh(2);
+  Array<double,2> & x = nodes1DProvisioner.get_xGrid();
+  Array<double,2> & Dr = nodes1DProvisioner.get_Dr();
+  Array<double,2> & V = nodes1DProvisioner.get_V();
 
-  //int * & epMap = mgr.get_ElementPartitionMap();
-  //int * & vpMap = mgr.get_VertexPartitionMap();
 
-  delete manager;
+  cout << Dr << endl;
+
+  cout << V << endl;
+
+  double min_dx = x(1,0) - x(0,0);
+
+  double dt = CFL*min_dx/c;
+
+  double u = 1;
+  while (t < finalTime) {
+
+    u = u - dt*c*u;
+
+    cout << u << endl;
+    t += dt;
+  }
+  
   return 0;
 }
