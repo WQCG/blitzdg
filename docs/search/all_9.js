0f69e52bf65b2dabea73922ff13589420b490ba2
Derek Steinmoeller <dsteinmo@gmail.com>
2018-05-14 16:21:35 -0400

    Refrehs docs.

--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['linalghelpers_2ehpp',['LinAlgHelpers.hpp',['../LinAlgHelpers_8hpp.html',1,'']]],
-  ['lusolver',['LUSolver',['../classLUSolver.html',1,'LUSolver'],['../classLUSolver.html#a50cc15a95f58dea28db5a66fff6d3e40',1,'LUSolver::LUSolver()']]]
+  ['lusolver',['LUSolver',['../classLUSolver.html',1,'LUSolver'],['../classLUSolver.html#a50cc15a95f58dea28db5a66fff6d3e40',1,'LUSolver::LUSolver()']]],
+  ['lusolver_2ehpp',['LUSolver.hpp',['../LUSolver_8hpp.html',1,'']]]
 ];
