cdf1d1698fd33015f4794588c0178c618f0091ca
Derek Steinmoeller <dsteinmo@gmail.com>
2018-05-20 10:28:52 -0400

    Refactor: Move Jacobi methods to their own class.

--- src/Nodes1DProvisioner.cpp
+++ src/Nodes1DProvisioner.cpp
@@ -38,7 +38,7 @@ namespace blitzdg {
         EToF{ new index_matrix_type(NumElements, NumFaces) },
         vmapM{ new index_vector_type(NumFacePoints*NumFaces*NumElements) },
         vmapP{ new index_vector_type(NumFacePoints*NumFaces*NumElements) },
-        MatrixConverter{}, EigSolver{}, LinSolver{}
+        MatrixConverter{}, EigSolver{}, LinSolver{}, Jacobi{}
     {}
 
     void Nodes1DProvisioner::buildNodes() {
@@ -47,7 +47,7 @@ namespace blitzdg {
 
         vector_type& r = *rGrid;
 
-        computeGaussLobottoPoints(alpha, beta, NOrder, r);
+        Jacobi.computeGaussLobottoPoints(alpha, beta, NOrder, r);
         
         buildVandermondeMatrix();
         buildDr();
@@ -293,7 +293,7 @@ namespace blitzdg {
 
         vector_type p(NOrder+1);
         for (index_type j=0; j <= NOrder; j++) {
-            computeJacobiPolynomial(*rGrid, 0.0, 0.0, j, p);
+            Jacobi.computeJacobiPolynomial(*rGrid, 0.0, 0.0, j, p);
             Vref(Range::all(), j) = p;
         }
     }
@@ -433,122 +433,13 @@ namespace blitzdg {
         delete vmapP; vmapP = nullptr;
     }
 
-    void Nodes1DProvisioner::computeJacobiPolynomial(vector_type const & x, real_type alpha, real_type beta, index_type N, vector_type & p) const {
-        Range all = Range::all();
-        index_type Np = (x.length())(0);
-
-        matrix_type pStorage(N+1, Np);
-
-        real_type gamma0 = pow(2,(alpha+beta+1))/(alpha+beta+1)*tgamma(alpha+1)*tgamma(beta+1)/tgamma(alpha+beta+1);
-
-        p = 1/sqrt(gamma0);
-        pStorage(0, all) = p;
-
-        if (N==0) 
-            return;
-
-        real_type gamma1 = (alpha+1)*(beta+1)/(alpha+beta+3)*gamma0;
-        p = ((alpha+beta+2)*x/2 + (alpha-beta)/2)/sqrt(gamma1);
-
-        pStorage(1, all) = p;
-
-        if (N==1) 
-            return;
-
-        real_type aold = 2/(2+alpha+beta)*sqrt((alpha+1)*(beta+1)/(alpha+beta+3));
-
-        // Forward recurrence using the symmetry of the recurrence.
-        for(index_type i=1; i <= N-1; i++) {
-            real_type h1 = 2*i+alpha+beta;
-            real_type anew = 2/(h1+2)*sqrt( (i+1)*(i+1+alpha+beta)*(i+1+alpha)*(i+1+beta)/(h1+1)/(h1+3));
-            real_type bnew = - (alpha*alpha-beta*beta)/h1/(h1+2);
-            pStorage(i+1,all) = 1/anew*( -aold*pStorage(i-1,all) + (x-bnew)*pStorage(i,all));
-            aold = anew;
-        }
-        p = pStorage(N, all);
-    }
-
-    void Nodes1DProvisioner::computeJacobiQuadWeights(real_type alpha, real_type beta, index_type N, vector_type& x, vector_type& w) const {
-
-        if ( N == 0) {
-            x(0) = -(alpha-beta)/(alpha+beta+2);
-            w(0) = 2.0;
-            return;
-        }
-
-        const real_type eps = numeric_limits<real_type>::epsilon();
-
-        firstIndex ii;
-        secondIndex jj;
-
-        // Form symmetric matrix.
-        matrix_type J(N+1,N+1);
-        J = 0.;
-        for (index_type i=0; i < N+1; i++) {
-            real_type h1 = 2.*i+alpha+beta;
-            J(i,i)   = -0.5*(alpha*alpha-beta*beta)/(h1+2.)/h1;
-            if (i < N) {
-                J(i,i+1) = 2./(h1+2)*sqrt((i+1)*((i+1)+alpha+beta)*((i+1)+alpha)*((i+1)+beta)/(h1+1)/(h1+3));
-            }
-        }
-
-        if ((alpha + beta) < 10*eps ) {
-            J(0,0) = 0.0;
-        }
-        J = J(ii,jj) + J(jj,ii);
-        
-        matrix_type eigenvectors(N+1, N+1);
-
-        EigSolver.solve(J, x, eigenvectors);
-
-        // The eigenvalues give the x points.
-        
-        // The weights are given by:
-        vector_type v1(N+1);
-        v1 = eigenvectors( 0, Range::all() ); 
-
-        real_type gamma0 = pow(2,(alpha+beta+1))/(alpha+beta+1)*tgamma(alpha+1)*tgamma(beta+1)/tgamma(alpha+beta+1);
-
-        w = (v1*v1)*gamma0;
-    }
-
-    void Nodes1DProvisioner::computeGaussLobottoPoints(real_type alpha, real_type beta, index_type N, vector_type& x) const {
-        if (N==1) {
-            x(0) = -1.0;
-            x(1) = 1.0;
-            return;
-        }
-
-        x(0) = -1.0;
-        x(N) = 1.0;
-
-        vector_type xJG(N-1);
-        vector_type w(N-1);
-
-        computeJacobiQuadWeights(alpha+1., beta+1., N-2, xJG, w);
-        
-        for(index_type i=1; i < N; i++)
-            x(i) = xJG(i-1);
-    }
-
-    void Nodes1DProvisioner::computeGradJacobi(vector_type const & x, real_type alpha, real_type beta, index_type N, vector_type& dp) const {
-        if (N == 0) {
-            dp = 0.0;
-            return;
-        }
-
-        vector_type p(x.length());
-
-        computeJacobiPolynomial(x, alpha+1, beta+1, N-1, p);
-        dp = sqrt(N*(N+alpha+beta+1))*p;
-    }
 
     void Nodes1DProvisioner::computeGradVandermonde(matrix_type & DVr) const {
         firstIndex ii;
         vector_type dp(NOrder+1);
         for (index_type i=0; i<=NOrder; i++) {
             dp = 0.*ii;
-            computeGradJacobi(*rGrid, 0.0, 0.0, i, dp);
+            Jacobi.computeGradJacobi(*rGrid, 0.0, 0.0, i, dp);
             DVr(Range::all(), i) = dp;
         }
     }
---
+++ src/test/JacobiBuildersTests.cpp
@@ -0,0 +1,183 @@
+// Copyright (C) 2017-2018  Waterloo Quantitative Consulting Group, Inc.
+// See COPYING and LICENSE files at project root for more details.
+
+#include "EigenSolver.hpp"
+#include "JacobiBuilders.hpp"
+#include "Types.hpp"
+#include <igloo/igloo_alt.h>
+
+using std::cout;
+using std::endl;
+using std::numeric_limits;
+
+namespace blitzdg {
+
+    namespace JacobiBuildersTests {
+        using namespace igloo;
+
+        const real_type eps = 10*numeric_limits<real_type>::epsilon();
+        const float epsf = 5.7e-6;
+
+        JacobiBuilders * Jacobi = nullptr;
+
+
+        Describe(JacobiBuilders_Object) {
+            void SetUp() {
+                Jacobi = new JacobiBuilders();        
+            }
+
+            void TearDown() {
+                delete Jacobi;
+            }
+
+            It(Should_Generate_0th_Order_Legendre_Polynomial) {
+                cout << "Should_Generate_0th_Order_Legendre_Polynomial" << endl;
+                vector_type x(3);
+                x = -1.,0.,1.;
+                vector_type  p(3);
+
+                const JacobiBuilders & jacobi = *Jacobi;
+
+                jacobi.computeJacobiPolynomial(x, 0.0, 0.0, 0, p);
+
+                Assert::That(p(0), Equals(1./sqrt(2.)));
+                Assert::That(p(1), Equals(1./sqrt(2.)));
+                Assert::That(p(2), Equals(1./sqrt(2.)));
+            }
+
+            It(Should_Generate_1st_Order_Legendre_Polynomial) {
+                cout << "Should_Generate_1st_Order_Legendre_Polynomial" << endl;
+                vector_type x(3);
+                x = -1.,0.,1.;
+                vector_type  p(3);
+
+                JacobiBuilders & jacobi = *Jacobi;
+
+                jacobi.computeJacobiPolynomial(x, 0.0, 0.0, 1, p);
+
+                Assert::That(p(0), Equals(-sqrt(3./2.)));
+                Assert::That(p(1), Equals(0.0));
+                Assert::That(p(2), Equals(sqrt(3./2.)));
+            }
+
+
+            It(Should_Generate_2nd_Order_Legendre_Polynomial) {
+                cout << "Should_Generate_2nd_Order_Legendre_Polynomial" << endl;
+                vector_type x(3);
+                x = -1.,0.,1.;
+                vector_type  p(3);
+
+                JacobiBuilders & jacobi = *Jacobi;
+
+                jacobi.computeJacobiPolynomial(x, 0.0, 0.0, 2, p);
+
+                Assert::That(abs(p(0)-sqrt(5./2.)), IsLessThan(eps));
+                Assert::That(abs(p(1)-(-sqrt(5./8.))), IsLessThan(eps));
+                Assert::That(abs(p(2)-sqrt(5./2.)), IsLessThan(eps));
+            }
+
+            It(Should_Generate_0th_Order_Legendre_Polynomial_4pt_Grid) {
+                cout << "Should_Generate_0th_Order_Legendre_Polynomial_4pt_Grid" << endl;
+                vector_type x(4);
+                x = -1,-0.447214,0.447214,1;
+                vector_type p(4);
+
+                JacobiBuilders & jacobi = *Jacobi;
+
+                jacobi.computeJacobiPolynomial(x, 0.0, 0.0, 0, p);
+
+                Assert::That(abs(p(0)-sqrt(1./2.)), IsLessThan(eps));
+                Assert::That(abs(p(1)-sqrt(1./2.)), IsLessThan(eps));
+                Assert::That(abs(p(2)-sqrt(1./2.)), IsLessThan(eps));
+            } 
+
+            It(Should_Generate_1st_Order_Legendre_Polynomial_4pt_Grid) {
+                cout << "Should_Generate_1st_Order_Legendre_Polynomial_4pt_Grid" << endl;
+                vector_type x(4);
+                x = -1,-0.447214,0.447214,1;
+                vector_type p(4);
+
+                JacobiBuilders & jacobi = *Jacobi;
+
+                jacobi.computeJacobiPolynomial(x, 0.0, 0.0, 1, p);
+
+                Assert::That(abs(p(0)- -1.224744871391589), IsLessThan(epsf));
+                Assert::That(abs(p(1)- -0.547722557505166), IsLessThan(epsf));
+                Assert::That(abs(p(2)-  0.547722557505166), IsLessThan(epsf));
+                Assert::That(abs(p(3)-  1.224744871391589), IsLessThan(epsf));
+            } 
+
+            It(Should_Generate_4th_Order_Quadrature_Points_and_Weights) {
+                cout << "Should_Generate_4th_Order_Quadrature_Points_and_Weights" << endl;
+                
+                JacobiBuilders & jacobi = *Jacobi;
+                
+                vector_type x(5);
+                vector_type w(5);
+
+                jacobi.computeJacobiQuadWeights(0., 0., 4, x, w);
+
+                Assert::That(abs(x(0) - -9.06179845938664e-01), IsLessThan(eps));
+                Assert::That(abs(x(1) - -5.38469310105683e-01), IsLessThan(eps));
+                Assert::That(abs(x(2) - -9.62591786604533e-17), IsLessThan(eps));
+                Assert::That(abs(x(3) -  5.38469310105683e-01), IsLessThan(eps));
+                Assert::That(abs(x(4) -  9.06179845938664e-01), IsLessThan(eps));
+
+                Assert::That(abs(w(0) - 0.236926885056189), IsLessThan(eps));
+                Assert::That(abs(w(1) - 0.478628670499366), IsLessThan(eps));
+                Assert::That(abs(w(2) - 0.568888888888889), IsLessThan(eps));
+                Assert::That(abs(w(3) - 0.478628670499367), IsLessThan(eps));
+                Assert::That(abs(w(4) - 0.236926885056189), IsLessThan(eps));
+            }
+
+            It(Should_Generate_1st_Order_Quadrature_Points_and_Weights) {
+                cout << "Should_Generate_1st_Order_Quadrature_Points_and_Weights" << endl;
+                
+                JacobiBuilders & jacobi = *Jacobi;
+                
+                vector_type x(2);
+                vector_type w(2);
+
+                jacobi.computeJacobiQuadWeights(0., 0., 1, x, w);
+
+                Assert::That(abs(x(0) - -0.577350269189626), IsLessThan(eps));
+                Assert::That(abs(x(1) -  0.577350269189626), IsLessThan(eps));
+
+                Assert::That(abs(w(0) - 1.0), IsLessThan(eps));
+                Assert::That(abs(w(1) - 1.0), IsLessThan(eps));
+            }
+
+            It(Should_Compute_2nd_Order_Quadrature_Points_and_Weights) {
+                cout << "Should_Compute_2nd_Order_Quadrature_Points_and_Weights" << endl;
+
+                JacobiBuilders & jacobi = *Jacobi;
+
+                vector_type xx(3);
+                vector_type ww(3);
+
+                jacobi.computeJacobiQuadWeights(0., 0., 2, xx, ww);
+
+                Assert::That(abs(xx(0) - -7.74596669241483e-01), IsLessThan(eps));
+                Assert::That(abs(xx(1) -  0.0), IsLessThan(eps));
+                Assert::That(abs(xx(2) -  7.74596669241483e-01), IsLessThan(eps));
+
+                Assert::That(abs(ww(0) -  0.555555555555556), IsLessThan(eps));
+                Assert::That(abs(ww(1) -  0.888888888888889), IsLessThan(eps));
+                Assert::That(abs(ww(2) -  0.555555555555556), IsLessThan(eps));
+            }
+
+            It(Should_Generate_3rd_Order_Legendre_Gauss_Lobatto_Nodes) {
+                cout << "Should_Generate_3rd_Order_Legendre_Gauss_Lobatto_Nodes" << endl;
+                JacobiBuilders & jacobi = *Jacobi;
+                
+                vector_type x(4);
+                jacobi.computeGaussLobottoPoints(0., 0., 3, x);
+
+                Assert::That(abs(x(0) - -1), IsLessThan(eps));
+                Assert::That(abs(x(1) - -0.447213595499958), IsLessThan(eps));
+                Assert::That(abs(x(2) -  0.447213595499958), IsLessThan(eps));
+                Assert::That(abs(x(3) -  1), IsLessThan(eps));
+            }
+        };
+    }
+}
