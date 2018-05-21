1b7c8c2e2e69d90e42cb6ae86f03d86455844adb
Derek Steinmoeller <dsteinmo@gmail.com>
2018-05-21 14:27:46 -0400

    Check-in matlab/octave script for plotting the advec1d output.

---
+++ visualization/plotter.m
@@ -0,0 +1,13 @@
+# Copyright (C) 2017-2018  Waterloo Quantitative Consulting Group, Inc.
+# See COPYING and LICENSE files at project root for more details.
+
+x = load('x.dat');
+
+files = glob('u*.dat');
+
+for ii=1:length(files)
+    u = load(files{ii});
+    plot(x(:),u(:));
+    drawnow;
+    pause(0.1);
+endfor
