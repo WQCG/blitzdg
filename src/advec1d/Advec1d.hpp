e76cd0682de10579ebcc95014d40c6536fa458cb
Derek Steinmoeller <dsteinmo@gmail.com>
2018-05-21 09:42:17 -0400

    Doc string and copyright notice.

--- src/advec1d/Advec1d.hpp
+++ src/advec1d/Advec1d.hpp
@@ -1,3 +1,10 @@
+// Copyright (C) 2017-2018  Waterloo Quantitative Consulting Group, Inc.
+// See COPYING and LICENSE files at project root for more details.
+
+/**
+ * @file Advec1d.hpp
+ * @brief Short header file for 1D advection solver's main.cpp file.
+ */
 #pragma once
 #include "Nodes1DProvisioner.hpp"
 #include "Types.hpp"
