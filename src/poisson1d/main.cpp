669172d492f9b431eaffa8f66f029c125e383bf8
Derek Steinmoeller <dsteinmo@gmail.com>
2018-05-31 11:04:06 -0400

    pull in the gmresser to poissond1d. Compiles and runs, but wrong answer.

--- src/poisson1d/main.cpp
+++ src/poisson1d/main.cpp
@@ -11,7 +11,7 @@
 #include "Warning.hpp"
 #include "Poisson1d.hpp"
 #include "LSERK4.hpp"
-#include "LinAlgHelpers.hpp"
+#include "GMRESSolver.hpp"
 #include <blitz/array.h>
 #include <cmath>
 #include <string>
@@ -22,9 +22,14 @@ using blitz::firstIndex;
 using blitz::secondIndex;
 using blitz::thirdIndex;
 using blitz::sum;
+using blitzdg::normMax;
+using blitzdg::poisson1d::PoissonOperator;
+using blitzdg::poisson1d::Precon;
+using blitzdg::fullToVector;
 using std::abs;
 using std::string;
 using std::cout;
+using std::endl;
 
 int main(int argc, char **argv) {
 	using namespace blitzdg;
@@ -35,8 +40,6 @@ int main(int argc, char **argv) {
 	// Numerical parameters:
 	const index_type N = 4;
 	const index_type K = 15;
-
-	const real_type tau = 1.0;
 
 	// Build dependencies.
 	Nodes1DProvisioner nodes1DProvisioner(N, K, xmin, xmax);
@@ -50,134 +53,54 @@ int main(int argc, char **argv) {
 
 	const real_matrix_type & x = nodes1DProvisioner.get_xGrid();
 
-	real_type min_dx = x(1,0) - x(0,0);
-
 	real_matrix_type u(Np, K);
 	real_matrix_type uexact(Np, K);
 	real_matrix_type RHS(Np, K);
+	real_matrix_type RHSexact(Np, K);
+
+	const index_type numGlobalNodes = Np*K;
+
+	real_vector_type RHSexactVec(numGlobalNodes);
+	real_vector_type outVec(numGlobalNodes);
 
 	firstIndex ii;
 	secondIndex jj;
+
+	outVec = 0*ii;
+
 
 	printDisclaimer();
 
 	// Intialize fields.
 	u = 0*jj;
 	uexact = sin(M_PI*x(ii,jj));
-	RHS = -M_PI*M_PI*sin(M_PI*x(ii,jj));
+	RHSexact = -M_PI*M_PI*sin(M_PI*x(ii,jj));
+
+	const bool byRowsOpt = false;
+
+	fullToVector(RHSexact, RHSexactVec, byRowsOpt);
 	
 	const char delim = ' ';
 	outputter.writeFieldToFile("x.dat", x, delim);
 
-	// Calculate Right-hand side.
-	poisson1d::applyPoissonOperator(u, nodes1DProvisioner, tau, RHS);
+	// Calculate Right-hand side for funs.
+	//poisson1d::applyPoissonOperator(uexact, nodes1DProvisioner, RHS);
+	GMRESSolver gmres;
+	GMRESParams params;
+	params.verbose = true;
+
+	GMRESOut result = gmres.solve(PoissonOperator(nodes1DProvisioner), Precon(), RHSexactVec, outVec, params);
+
+	vectorToFull(outVec, u, byRowsOpt);
+	cout << result << endl;
+	cout << u << endl;
+
+	outputter.writeFieldToFile("u.dat", u, delim);
+	outputter.writeFieldToFile("uexact.dat", u, delim);
+	outputter.writeFieldToFile("RHS.dat", RHSexact, delim);
+
+	u -= uexact;
+	cout << normMax(u) << endl;
 
 	return 0;
 } // end main
-
-namespace blitzdg {
-	namespace poisson1d {
-		void applyPoissonOperator(const real_matrix_type & u, const Nodes1DProvisioner & nodes1D, const real_type tau, real_matrix_type & result) {
-			// Blitz indices
-			firstIndex ii;
-			secondIndex jj;
-			thirdIndex kk;
-			const real_matrix_type& Dr = nodes1D.get_Dr();
-			const real_matrix_type& rx = nodes1D.get_rx();
-			const real_matrix_type& Lift = nodes1D.get_Lift();
-			const real_matrix_type& Fscale = nodes1D.get_Fscale();
-			const real_matrix_type& nx = nodes1D.get_nx();
-			const real_matrix_type& Vinv = nodes1D.get_Vinv();
-
-			// Get volume to surface maps.
-			const index_vector_type& vmapM = nodes1D.get_vmapM();
-			const index_vector_type& vmapP = nodes1D.get_vmapP();
-
-			// boundary indices.
-			index_type mapO = nodes1D.get_mapO();
-			index_type mapI = nodes1D.get_mapI();
-
-			index_type numFaces = nodes1D.NumFaces;
-			index_type Nfp = nodes1D.NumFacePoints;
-			index_type Np = nodes1D.get_NumLocalPoints();
-			index_type K = nodes1D.get_NumElements();
-
-			real_matrix_type VinvTrans(Np, Np);
-			VinvTrans = Vinv(jj,ii);
-
-			// Construct mass matrix (On Standard element - No Jacobian scaling).
-			real_matrix_type MassMatrix(Np,Np);
-			MassMatrix = Vinv(ii,kk)*Vinv(jj,kk);
-
-			real_vector_type du(numFaces*Nfp*K);
-			real_vector_type uM(numFaces*Nfp*K);
-			real_vector_type uP(numFaces*Nfp*K);
-			real_vector_type nxVec(numFaces*Nfp*K);
-
-			du = 0.*ii;
-			uM = 0.*ii;
-			uP = 0.*ii;
-			nxVec = 0.*ii;
-
-			real_matrix_type nxCol(numFaces*Nfp,K, ColumnMajorArray<2>());
-			nxCol = nx;		
-
-			real_matrix_type uCol(Np, K, ColumnMajorArray<2>());
-			uCol = u;
-
-			real_matrix_type uxCol(Np, K, ColumnMajorArray<2>());
-			uxCol = rx*sum(Dr(ii,kk)*u(kk,jj), kk);
-
-			// Get + and - traces at element interfaces
-			index_type count = 0;
-			for( index_type k=0; k < K; k++) {
-				for ( index_type f=0; f < Nfp*numFaces; f++) {
-					nxVec(count) = nxCol(f,k);
-					uM(count) = uCol(vmapM(count));
-					uP(count) = uCol(vmapP(count));
-					count++;
-				}
-			}
-
-			// impose boundary condition -- Dirichlet BC's
-			real_type uin  = -uM(mapI);
-			real_type uout = -uM(mapO);
-
-			uP(mapI) = uin;
-			uP(mapO) = uout;
-
-			// Compute jump in flux:
-			du = (uM - uP);
-
-			real_matrix_type duMat(Nfp*numFaces, K);
-
-			count = 0;
-			for( index_type k=0; k < K; k++) {
-				for ( index_type f=0; f < Nfp*numFaces; f++) {
-					duMat(f,k) = du(count);
-					count++;
-				}
-			}
-
-			// Auxiliary variable to hold first derivative.
-			real_matrix_type q(Np, K);
-			q = rx*sum(Dr(ii,kk)*u(kk,jj), kk);
-
-			real_matrix_type surfaceRHS(Nfp*numFaces, K);
-			surfaceRHS = Fscale*duMat;
-			q -= sum(Lift(ii,kk)*surfaceRHS(kk,jj), kk);
-
-			// Get + and - traces at element interfaces
-			index_type count = 0;
-			for( index_type k=0; k < K; k++) {
-				for ( index_type f=0; f < Nfp*numFaces; f++) {
-					nxVec(count) = nxCol(f,k);
-					uM(count) = uCol(vmapM(count));
-					uP(count) = uCol(vmapP(count));
-					count++;
-				}
-			}
-
-		} // computeRHS
-	} // namespace poisson1d
-} // namespace blitzdg
