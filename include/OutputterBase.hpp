0418326ea02003e0d93198428fa75b5c890e8e7f
Derek Steinmoeller <dsteinmo@gmail.com>
2019-02-02 08:18:05 -0500

    Check in OutputterBase.hpp.

---
+++ include/OutputterBase.hpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2017-2019  Waterloo Quantitative Consulting Group, Inc.
+// See COPYING and LICENSE files at project root for more details.
+
+#pragma once
+#include "Types.hpp"
+#include <string>
+#include <map>
+
+namespace blitzdg {
+    class OutputterBase {
+    public:
+        virtual void writeFieldsToFiles(std::map<std::string, real_matrix_type>& fields, index_type tstep) = 0;
+    };
+}
