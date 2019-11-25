b7ef94a33d1b291fd8afe79ae5d720f2eaea96aa
Derek Steinmoeller <dsteinmo@gmail.com>
2019-11-24 20:09:03 -0500

    Implement the splitElements method for Quads provisioner.

--- src/QuadNodesProvisioner.cpp
+++ src/QuadNodesProvisioner.cpp
@@ -710,7 +710,120 @@ namespace blitzdg {
     void QuadNodesProvisioner::splitElements(const real_matrix_type& x, const real_matrix_type& y, const real_matrix_type& field,
         real_matrix_type& xnew, real_matrix_type& ynew, real_matrix_type& fieldnew) const {
 
-        throw std::runtime_error("not implemented yet.");
+        index_type Np = field.rows();
+		index_type K = field.cols();
+
+		real_vector_type rout(Np), sout(Np);
+
+		const index_type N = NumFacePoints - 1;
+
+		index_type count = 0;
+
+		index_matrix_type counter(N+1,N+1);
+		counter = -1; // -1 == 'No Value'
+
+		for (index_type n=0; n < N+1; ++n) {
+			for (index_type m=0; m < N+1; ++m) {
+				rout(count) = -1. + 2.*static_cast<double>(m)/static_cast<double>(N);
+				sout(count) = -1. + 2.*static_cast<double>(n)/static_cast<double>(N);
+
+				counter(n,m) = count;
+				++count;
+			}
+		}
+
+		real_matrix_type IM(Np,Np);
+		IM = 0.;
+
+		computeInterpMatrix(rout, sout, IM);
+
+		vector<index_vector_type> localE2V;
+
+		index_type numLocalElements =0;
+		for (index_type n=0; n < N; ++n) {
+			for (index_type m=0; m < N; ++m) {
+				index_type v1 = counter(n,m), v2 = counter(n,m+1),
+					v3 = counter(n+1, m), v4 = counter(n+1,m+1);
+
+				index_vector_type quad1234(4);
+				quad1234 = v1,v2,v3,v4;
+				
+				localE2V.push_back(quad1234);
+				//if (v4 >= 0) {
+				//	index_vector_type tri243(3);
+				//	tri243 = v2,v4,v3;
+				//	localE2V.push_back(tri243);
+				//	++numLocalElements;
+				//}
+			
+				++numLocalElements;
+			}
+		}
+
+		vector<index_vector_type> E2Vnew;
+
+		for (index_type k=0; k<K; ++k) {
+			index_type shift = k*Np;
+
+			for (index_type l=0; l<numLocalElements; ++l) {
+				index_vector_type row(4);
+				row(0) = localE2V[l](0) + shift;
+				row(1) = localE2V[l](1) + shift;
+				row(2) = localE2V[l](2) + shift;
+                row(3) = localE2V[l](3) + shift;
+				E2Vnew.push_back(row);
+			}
+		}
+
+		index_type totalNewElements = numLocalElements*K;
+
+		blitz::firstIndex ii;
+		blitz::secondIndex jj;
+		blitz::thirdIndex kk;
+
+		real_matrix_type resultx(Np,K), resulty(Np,K), resultField(Np,K);
+		resultx = blitz::sum(IM(ii,kk)*x(kk,jj),kk);
+		resulty = blitz::sum(IM(ii,kk)*y(kk,jj),kk);
+		resultField = blitz::sum(IM(ii,kk)*field(kk,jj),kk);
+		
+
+		real_vector_type xVec(Np*K), yVec(Np*K), fieldVec(Np*K);
+		fullToVector(resultx, xVec, false);
+		fullToVector(resulty, yVec, false);
+		fullToVector(resultField, fieldVec, false);
+
+		// Unpack 1D arrays storing EToV and Vertex coordinates
+		index_vector_type va(totalNewElements), vb(totalNewElements), vc(totalNewElements), vd(totalNewElements);
+		for (index_type i=0; i < totalNewElements; ++i) {
+				va(i) = E2Vnew[i](0);
+				vb(i) = E2Vnew[i](1);
+				vc(i) = E2Vnew[i](2);
+                vd(i) = E2Vnew[i](3);
+		}
+
+		// resize arrays for the new linear elements.
+		xnew.resize(4, totalNewElements);
+		ynew.resize(4, totalNewElements);
+		fieldnew.resize(4, totalNewElements);
+
+		for (index_type i=0; i < totalNewElements; ++i) {
+			 index_type vai = va(i), vbi = vb(i), vci = vc(i), vdi = vd(i);
+
+				xnew(0,i) = xVec(vai);
+				xnew(1,i) = xVec(vbi);
+				xnew(2,i) = xVec(vci);
+                xnew(3,i) = xVec(vdi);
+
+				ynew(0,i) = yVec(vai);
+				ynew(1,i) = yVec(vbi);
+				ynew(2,i) = yVec(vci);
+                ynew(3,i) = yVec(vdi);
+				
+				fieldnew(0,i) = fieldVec(vai);
+				fieldnew(1,i) = fieldVec(vbi);
+				fieldnew(2,i) = fieldVec(vci);
+                fieldnew(3,i) = fieldVec(vdi);
+		}
     }
 
     DGContext2D QuadNodesProvisioner::get_DGContext() const {
