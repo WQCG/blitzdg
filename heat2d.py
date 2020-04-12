b5e57f6a640e7b9bd1e9e6aafa863fd6ea4f93c2
Derek Steinmoeller <dsteinmo@gmail.com>
2020-04-12 15:59:53 -0400

    Change and clean up heat2d.py for doing both Dirichlet and Neuman BC's as well as specifying the BC functions for the trapezoid problem.

--- heat2d.py
+++ heat2d.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Copyright (C) 2017-2019  Waterloo Quantitative Consulting Group, Inc.
+Copyright (C) 2017-2020  Waterloo Quantitative Consulting Group, Inc.
 See COPYING and LICENSE files at project root for more details.
 '''
 
@@ -9,70 +9,160 @@ from pyblitzdg import pyblitzdg as dg
 import matplotlib.pyplot as plt
 from scipy.sparse import csc_matrix, eye
 from scipy.sparse.linalg import splu as lu
+import cghelpers as cg
 
 meshManager = dg.MeshManager()
-meshManager.readMesh('./input/box.msh')
+meshManager.readMesh('./input/trapezoid.msh')
+
+Verts = meshManager.vertices
+EToV = meshManager.elements
+bcType = meshManager.bcType
+
+
+# Correct boundary condition table for the different types
+# TODO, store magic integers like 6 - Dirichlet and 7 - Neuman in an enum prop.
+faces = np.array([[0, 1], [1, 2], [2, 0]])
+for e in range(0, len(bcType)):
+    for (faceInd, f) in enumerate(faces):
+        v1 = EToV[e, f[0]]
+        v2 = EToV[e, f[1]]
+        v1x = Verts[v1, 0]
+        v1y = Verts[v1, 1]
+        v2x = Verts[v2, 0]
+        v2y = Verts[v2, 1]
+
+        midx = 0.5*(v1x + v2x)
+        midy = 0.5*(v1y + v2y)
+
+        if  np.abs(midy - 1.3) < 1e-12:
+            bcType[e, faceInd] = 6
+        elif np.abs(midy) < 1e-12:
+            bcType[e, faceInd] = 7   # 7 - bottom neuman
+        elif np.abs(midy - 1.3*midx) < 1e-12:
+            bcType[e, faceInd] = 6
+        elif np.abs(midy + 1.3*midx - 26/5) < 1e-12:
+            bcType[e, faceInd] = 6
+
+# Update BC table in the backend
+meshManager.setBCType(bcType)
+
+#set diffusivity param
+kappa = 1
+#kappa = 1.0 - 0.9*np.exp(-100*(x-2.0)**2)
 
 # Numerical parameters:
-N = 1
+N = 3
 
 nodes = dg.TriangleNodesProvisioner(N, meshManager)
 ctx = nodes.dgContext()
 x = ctx.x
 y = ctx.y
+outputter = dg.VtkOutputter(nodes)
 
-kappa = 0.05
-dt = 0.005
+CFL = 0.85
 
+dt = CFL / np.max( ((N+1)**2)*0.5*(np.abs(ctx.Fscale.flatten('F'))**2)*kappa)
+
+Np = ctx.numLocalPoints
+K = ctx.numElements
+Nfaces = ctx.numFaces
+Nfp = ctx.numFacePoints
+
+# identify boundary face nodes
+vmapM = ctx.vmapM
+
+bcMap = ctx.BCmap
+mapD = bcMap[6]
+mapN = bcMap[7]
+
+Fx = x.flatten('F')[vmapM]
+Fy = y.flatten('F')[vmapM]
+
+bottomLeft = np.where(np.abs(Fx) < 1e-12)[0]
+
+top  = np.where(np.abs(Fy - 1.3) < 1e-12)[0]
+left = np.where(np.abs(Fy - 1.3*Fx) < 1e-12)[0]
+right = np.where(np.abs(Fy + 1.3*Fx - 26/5) < 1e-12)[0]
+
+bottom = np.where(np.abs(Fy) < 1e-12)[0] #chop first and last points from neuman bc
+bottomNoRight = np.where(Fx[bottom] < 4)
+bottom = bottom[bottomNoRight]
+
+bcType = meshManager.bcType
+
+EToV = meshManager.elements
+Verts = meshManager.vertices
+
+# Constructing poisson2D sparse matrix
 poisson2d = dg.Poisson2DSparseMatrix(ctx, meshManager)
 
+#Neuman - homogeneous
+qbc = np.zeros((Nfp*Nfaces*K))
+
+# Dirichlet BCs
+ubc = np.zeros((Nfp*Nfaces*K))
+ubc[right] = np.ones(right.shape) - (10/13)*Fy[right]
+ubc[top]   = 0.25*(np.exp(4*(Fx[top]-3)) - 1)
+ubc[left]  = -0.25*np.ones(left.shape)
+
+# Put in the shape required for the backend.
+qbc = np.reshape(qbc, (Nfp*Nfaces, K), order='F')
+ubc = np.reshape(ubc, (Nfp*Nfaces, K), order='F')
+
+# Compute RHS
+bcRhs = poisson2d.buildBcRhs(ctx, meshManager, np.array(ubc, dtype=np.float64, order='C'), np.array(qbc, dtype=np.float64, order='C'))
+
+# get sparse triplet global operators
 MM = poisson2d.getMM()
 OP = poisson2d.getOP()
 
-# create scipy sparse mats
+# create scipy sparse matrices.
 dof = ctx.numLocalPoints*ctx.numElements
-MMsp = csc_matrix((MM[:, 2], (MM[:, 0], MM[:, 1])), shape=(dof, dof), dtype=np.dtype('Float64'))
+MMsp = csc_matrix((MM[:, 2], (MM[:, 0], MM[:, 1])), shape=(dof, dof), dtype=np.float64)
 
 rows = OP[:, 0]
 cols = OP[:, 1]
 vals = OP[:, 2]
 
-vals = kappa*dt*(-vals)
+# scale laplacian assuming const dt.
+vals = kappa*dt*(vals)
 
 # Pass sparse triplet into csc_matrix constructor.
 scaledLap = csc_matrix((vals, (rows, cols)), shape=(dof, dof))
 
-OPsp = MMsp - scaledLap
+# form full LHS operator.
+OP = MMsp + scaledLap
 
 # factorize
-linSolver = lu(OPsp)
+linSolver = lu(OP)
 
 # Define initial condition.
-u = 2.5*np.exp(-((x/0.1)**2.0 + (y/0.1)**2.0), dtype=np.dtype('Float64'), order='C')
+p = np.zeros((Np, K))
 
-for j in range(0, 500):
+bcContrib = bcRhs.flatten('F')
 
-    # compute load vector (MassMatrix times u)
-    rhs = u.flatten('F')
-    rhs = MMsp.dot(rhs)
+fields = dict()
+fields["p"] = np.array(p, dtype=np.float64, order='C')
+outputter.writeFieldsToFiles(fields, 0)
+
+for j in range(1, 2000):
+
+    # compute load vector (MassMatrix times p)
+    rhs = p.flatten('F')
+    MMrhs = MMsp.dot(rhs)
 
     # solve x = A \ b
-    soln = linSolver.solve(rhs)
+    soln = linSolver.solve(MMrhs + bcContrib)
     soln = np.reshape(soln, (ctx.numLocalPoints, ctx.numElements), order='F')
-
-    # Output solution statistics.
-    #print(f"max: {np.max(soln)}")
-    #print(f"min: {np.min(soln)}")
-    #print(f"mean: {np.sum(soln)/dof}")
 
     # output every 10th timestep
     if j % 10 == 0:
-        fields = dict()
+        print("Writing output at t=" + str(dt*j))
+
         # Need to ensure fields send to the outputter are Row-Major ('C'-ordering),
         # since blitzdg internally assumes they are.
-        fields["u"] = np.array(soln, dtype=np.dtype('Float64'), order='C')
-        outputter = dg.VtkOutputter(nodes)
+        fields["p"] = np.array(soln/np.max(np.abs(soln)), dtype=np.float64, order='C')
         outputter.writeFieldsToFiles(fields, j)
 
     # rotate soln and initial state for next timestep
-    u = soln
+    p = soln
